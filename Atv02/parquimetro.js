7f3b35cf970c566f94b381bad5e6ceaf37097036
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-06-15 22:41:26 -0300

    first commit

--- Atv02/parquimetro.js
+++ Atv02/parquimetro.js
@@ -6,50 +6,55 @@ const outTroco = document.querySelector("h5")
 
 const inValor = document.getElementById("inValor");
 
-//adicionando uma ação de click ao button
-
+//Adicionando uma ação de click ao button
 frm.addEventListener("submit", (e) => {
 
-    let tempo = 0;
-  
-    outTempo.innerText = "";
-    outTroco.innerText = "";
-  
-    let valor = parseFloat(inValor.value); // Obtém o valor digitado e converte para um número float
-  
-    //evita o envio do formulário
-    e.preventDefault();
-  
-    //Verificar se os valores são números válidos
-    if (valor === 0 || isNaN(valor)) {
-      alert("Digite um valor corretamente");
-      inValor.value = ""; //Limpa o campo de entrada
-      inValor.focus(); //Dá foco ao campo de entrada novamente
+  let tempo = 0
+  let resto = 0
+  let vezes = 0
 
-    } else {
-      if (valor >= 3) { // Verifica se o valor é maior ou igual a 3
-        tempo += 120;
-        valor -= 3;
+  outTempo.innerText = ""
+  outTroco.innerText = ""
 
-      } else if (valor >= 1.75) { // Verifica se o valor é maior ou igual a 1.75
-        tempo += 60;
-        valor -= 1.75;
+  let valor = Number(frm.inValor.value);
 
-      } else if (valor >= 1) { // Verifica se o valor é maior ou igual a 1
-        tempo += 30;
-        valor -= 1;
-      }
-  
-      inValor.value = ""; //Limpa o campo de entrada
-      inValor.focus(); //Dá foco ao campo de entrada novamente
-  
-      if (tempo === 0) {
-        outTempo.innerText = "Valor Insuficiente";
-      } else {
-        outTempo.innerText = "Minutos Disponíveis: " + tempo;
-        outTroco.innerText = "Troco: R$ " + valor.toFixed(2); //toFixed() arredonda o número para cima
-      }
+  //evita o envio do formulario
+  e.preventDefault();
+
+  //Verificar se os valores são números válidos
+  if (valor == 0) {
+    alert("Digite um valor corretamente");
+    inValor.value = ""; //Limpa o campo de entrada
+    inValor.focus(); //Dá foco ao campo de entrada novamente
+
+  } else {
+
+    if (valor >= 3) { // Verifica se o valor é maior ou igual a 3
+      resto = valor % 3 
+      vezes = (valor - resto) / 3
+      tempo = tempo + (vezes * 120)
+      valor = resto
     }
 
+    if (valor >= 1.75) { // Verifica se o valor é maior ou igual a 1.75
+      resto = valor % 1.75
+      vezes = (valor - resto) / 1.75
+      tempo = tempo + (vezes * 60)
+      valor = resto
+    }
+
+    if (valor >= 1) { // Verifica se o valor é maior ou igual a 1
+      resto = valor % 1
+      vezes = (valor - resto) / 1
+      tempo = tempo + (vezes * 30)
+      valor = resto
+    }
+
+    inValor.value = "" //Limpa o campo de entrada
+    inValor.focus() //Dá foco ao campo de entrada novamente
+
+    outTempo.innerText = "Minutos Disponíveis: " + tempo
+    outTroco.innerText = "Troco: R$ " + resto.toFixed(2) //toFixed() arredonda o número para cima
+  }
 
 })
