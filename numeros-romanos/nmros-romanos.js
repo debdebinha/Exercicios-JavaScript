721830110456d75235724938c39494344cc0cbd0
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-06-23 18:47:19 -0300

    finished nmros-romanos.js

--- numeros-romanos/nmros-romanos.js
+++ numeros-romanos/nmros-romanos.js
@@ -1,6 +1,6 @@
-const frm = document.querySelector("form")
-const resultElement = document.querySelector("h4")
-const romanInput = document.getElementById("romanInput");
+const frm = document.querySelector("form") // Obtendo o formulário do HTML
+const resultElement = document.querySelector("h4") // Obtendo o elemento <h4> para exibir o resultado
+const romanInput = document.getElementById("romanInput"); // Obtendo o campo de entrada do número romano
 
 //inicializando uma função ao clicar no botão
 frm.addEventListener("submit", (e) => {
@@ -12,12 +12,11 @@ frm.addEventListener("submit", (e) => {
     let romanNumber = romanInput.value.toUpperCase(); // .toUpperCase para deixar os números romanos em letra Maiúscula.
 
     // Verificando se a entrada está vazia
-    if (!romanNumber) {
-        resultElement.textContent = "Entrada inválida - Por favor, insira um número romano.";
+    if (romanNumber === "") {
+        resultElement.innerText = "Entrada inválida - Por favor, insira um número romano.";
         return;
     }
 
-    // Convertendo um número romano em decimal usando um loop while
     const romanValues = {
         I: 1,
         V: 5,
@@ -31,18 +30,33 @@ frm.addEventListener("submit", (e) => {
     let decimalNumber = 0;
     let i = 0;
 
+    // Convertendo um número romano em decimal usando um loop while
     while (i < romanNumber.length) { // while vai percorrer o número romano, realizando adições ou subtrações com base na comparação dos algarismos adjacentes
-      const currentRomanValue = romanValues[romanNumber[i]]; // Valor do algarismo romano atual
-      const nextRomanValue = romanValues[romanNumber[i + 1]]; // Valor do próximo algarismo romano
-  
-      if (nextRomanValue && currentRomanValue < nextRomanValue) {
-        decimalNumber += nextRomanValue - currentRomanValue; // Subtração: adiciona a diferença entre os valores do próximo algarismo e do algarismo atual
-        i += 2; // Pula dois caracteres (valor atual e próximo)
-      } else {
-        decimalNumber += currentRomanValue; // Adição: adiciona o valor do algarismo atual
-        i++; // Pula apenas um caractere (valor atual) para verificar o próximo algarismo
-      }
+        const currentRomanValue = romanValues[romanNumber[i]]; // Valor do algarismo romano atual
+
+        // Verificando se o valor atual não está definido em romanValues
+        if (currentRomanValue === undefined) {
+            resultElement.innerText = "Valor inválido - Por favor, insira um número romano válido.";
+
+            romanInput.value = ""; //Limpa o campo de entrada
+            romanInput.focus(); //Dá foco ao campo de entrada novamente
+
+            return;
+        }
+
+        const nextRomanValue = romanValues[romanNumber[i + 1]]; // Valor do próximo algarismo romano
+
+        if (nextRomanValue && currentRomanValue < nextRomanValue) {
+            decimalNumber += nextRomanValue - currentRomanValue; // Subtração: adiciona a diferença entre os valores do próximo algarismo e do algarismo atual
+            i += 2; // Pula dois caracteres (valor atual e próximo)
+        } else {
+            decimalNumber += currentRomanValue; // Adição: adiciona o valor do algarismo atual
+            i++; // Pula apenas um caractere (valor atual) para verificar o próximo algarismo
+        }
     }
+
+    romanInput.value = ""; //Limpa o campo de entrada
+    romanInput.focus(); //Dá foco ao campo de entrada novamente
 
     // Exibindo o resultado na tela
     resultElement.innerText = `O número decimal correspondente a "${romanNumber}" é: ${decimalNumber}`;
